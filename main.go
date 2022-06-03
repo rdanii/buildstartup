5513bfdfc4eac0fff675580e6e7c36f4d6345435
rdanii <dani.rahmat7398@gmail.com>
2022-06-03 19:29:02 +0700

    init project. Register user endpoint

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bwastartup/handler"
+	"bwastartup/user"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	dsn := "root:@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	userRepository := user.NewRepository(db)
+	userService := user.NewService(userRepository)
+
+	userHandler := handler.NewUserHandler(userService)
+
+	r := gin.Default()
+	api := r.Group("/api/v1")
+
+	api.POST("/users", userHandler.RegisterUser)
+	r.Run(":8000")
+
+	// input dari user
+	// handler, mapping input dari user -> struct input
+	// service : melakukan mapping dari struct input ke struct User
+	// repository
+	// db
+}
